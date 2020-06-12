24d9c9623d8c08a6b1750753101b418254e8b22d
Ian Lin <ian.lin@yjtech.tw>
2020-06-12 19:23:07 +0800

    [modify] about okhttp

--- repository/src/main/java/com/example/repository/LoggingInterceptor.kt
+++ repository/src/main/java/com/example/repository/LoggingInterceptor.kt
@@ -10,18 +10,18 @@ class LoggingInterceptor : Interceptor {
         private val TAG = LoggingInterceptor::class.java.simpleName
     }
 
-    override fun intercept(chain: Interceptor.Chain?): Response {
-        val request = chain?.request()
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
 
         val t1 = System.nanoTime()
         Log.d(TAG, String.format("Sending request %s on %s%n%s",
-            request?.url(), chain?.connection(), request?.headers()))
+                                 request.url, chain.connection(), request.headers))
 
-        val response = chain?.proceed(request)
+        val response = chain.proceed(request)
 
         val t2 = System.nanoTime()
         Log.d(TAG, String.format("Received response for %s in %.1fms%n%s",
-            response?.request()?.url(), (t2 - t1) / 1e6, request?.headers()))
+                                 response.request.url, (t2 - t1) / 1e6, request.headers))
 
         return response!!
     }
